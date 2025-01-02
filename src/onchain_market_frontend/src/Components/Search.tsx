6abff083eb399a2ec498d71cb8260523dd093dbe
Devdannetworks <officialdevduncan@gmail.com>
2025-01-02 12:21:51 +0300

    Initial commit

--- src/onchain_market_frontend/src/Components/Search.tsx
+++ src/onchain_market_frontend/src/Components/Search.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { CiSearch } from "react-icons/ci";
+
 const Search = () => {
-  return <div></div>;
+  const [loading, setLoading] = useState(false);
+  return (
+    <div className="flex w-full max-w-sm items-center ">
+      <Input
+        type="search"
+        placeholder="search markets"
+        className="rounded-r-none"
+      />
+      <div className="bg-slate-600 h-[36px] pl-1 pr-1 flex flex-col items-center justify-center rounded-r-md">
+        <h5>Search</h5>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
--- src/onchain_market_frontend/src/Header/Navigation/nav.tsx
+++ src/onchain_market_frontend/src/Header/Navigation/nav.tsx
@@ -2,10 +2,12 @@ import Heading from "@/Components/Heading";
 import Container from "../../Components/Container";
 import { Button } from "@/components/ui/button";
 import logo1 from "@/images/logo.svg";
+import { CiSearch } from "react-icons/ci";
+import Search from "@/Components/Search";
 
 const Nav = () => {
   return (
-    <Container bg_color={"bg-[#2596be]"}>
+    <Container bg_color={"bg-[#11122D]"}>
       <div className="flex justify-between items-center ">
         <div className=" flex items-center h-[65px] w-[240px]">
           <img
@@ -15,11 +17,14 @@ const Nav = () => {
           />
         </div>
         <div>
-          <h5>Chain market</h5>
+          <Search />
         </div>
 
-        <div className="flex  items-center text-center">
-          <Button>Click me!</Button>
+        <div className="flex  items-center text-center space-x-2">
+          <Button className="border-white border bg-transparent">
+            Sign up
+          </Button>
+          <Button variant="secondary">Log in</Button>
         </div>
       </div>
     </Container>
